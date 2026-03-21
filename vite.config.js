56509e2c00d25e136f1fdca5781589d4d6cd77cb
kevg1973 <kevg1973@gmail.com>
2026-03-21 10:45:55 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 5173,
-    host: true
-  },
   preview: {
     port: process.env.PORT || 4173,
-    host: true
+    host: true,
+    allowedHosts: ['leanplan-production.up.railway.app']
   }
 })
